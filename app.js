25a0dd86800260fa8ec47a9ed03efd77ac99456b
Arthur Leony <138785512+aArthurleony@users.noreply.github.com>
2023-11-21 12:29:44 -0300

    Add files via upload
    aaaaa

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-function validaFormulario(){
-    const nome = document.getElementById ("nome").value;
-    const email = document.querySelector ("#email").value;
-    const senha = document.getElementById ("senha").value;
+function validaformulario (){
+    const nome = document.getElementById("nome").value;
+    const email = document.querySelector("#email").value;
+    const senha = document.getElementById("senha").value;
     
     /*
     validação dos campos preenchidos
     */
 
-    if(nome == "" || email == "" || senha == ""){
+    if(nome == '' || email == '' || senha == ''){
         alert("Por favor, preencha os campos")
     }
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
         if(!emailRegex.test(email)){
             alert("Por favor, insira um e-mail válido!")
             return
-        } 
+    } 
     //validação da senha
     if(senha.length > 8){
         alert("Limite de carácteres excedido!")
